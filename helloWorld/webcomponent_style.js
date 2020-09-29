6b364eb3257d91d2c509cfd649c4fa7a60149b9a
Marie Klever <marieklever@live.no>
2020-09-29 15:36:40 +0200

    Add files via upload

--- helloWorld/webcomponent_style.js
+++ helloWorld/webcomponent_style.js
@@ -35,7 +35,7 @@
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
-            this.redraw();
+           // this.redraw();
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
