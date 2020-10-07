7244497575a5fb09c919b4efab1f676ba38f6934
Marie Klever <marieklever@live.no>
2020-10-07 15:18:45 +0200

    Add files via upload

--- multicombobox/webcomponent.js
+++ multicombobox/webcomponent.js
@@ -6,7 +6,7 @@
     tmpl.innerHTML = `
         <style>
 
-        .myStyle {
+        .sapMInputBaseContentWrapper.myStyle {
             border: 1px solid #0083A3;
             background: #ffffff;
         }
